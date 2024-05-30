0390a50471bf88389a3bbf9b1f84e2dcfb3fdf7d
extsui <en422641@s.okayama-u.ac.jp>
2024-05-30 23:08:01 +0900

    Serial.begin() 直後にログを出すと不安定になるので暫定で待ちを追加

--- Samples/EspNowMaster/src/main.cpp
+++ Samples/EspNowMaster/src/main.cpp
@@ -221,6 +221,11 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 void setup()
 {
     Serial.begin(115200);
+    
+    // USB-CDC のせいか起動直後にログを大量に出しても PC 側に表示されない
+    // 適当なディレイを入れると安定するようになったので暫定対処
+    delay(1000);
+
     //Set device in STA mode to begin with
     WiFi.mode(WIFI_STA);
     LOG("ESPNow/Multi-Slave/Master Example\n");
