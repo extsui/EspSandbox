958f2cab7315b30a894e4d34749d103271a56434
extsui <en422641@s.okayama-u.ac.jp>
2024-06-03 23:07:05 +0900

    (Fountain) DIPSW を自身のアドレスとして使用できる機能を追加

--- Samples/Fountain/src/Main.cpp
+++ Samples/Fountain/src/Main.cpp
@@ -1,13 +1,54 @@
 #include <Arduino.h>
+#include <stdint.h>
 #include "Util.h"
+
+namespace Pin {
+
+constexpr int DipSwitch0 = D2;
+constexpr int DipSwitch1 = D3;
+constexpr int DipSwitch2 = D4;
+constexpr int DipSwitch3 = D5;
+
+}
+
+namespace {
+
+uint8_t g_OwnAddress = 0xFF;
+
+void InitializeDipSwitch() noexcept
+{
+    pinMode(Pin::DipSwitch0, INPUT_PULLUP);
+    pinMode(Pin::DipSwitch1, INPUT_PULLUP);
+    pinMode(Pin::DipSwitch2, INPUT_PULLUP);
+    pinMode(Pin::DipSwitch3, INPUT_PULLUP);
+}
+
+// TODO: チャタリング除去
+// TODO: 動的アドレス変更対応
+uint8_t ReadOwnAddress() noexcept
+{
+    // 全て負論理
+    uint8_t d0 = ~digitalRead(Pin::DipSwitch0) & 0x1;
+    uint8_t d1 = ~digitalRead(Pin::DipSwitch1) & 0x1;
+    uint8_t d2 = ~digitalRead(Pin::DipSwitch2) & 0x1;
+    uint8_t d3 = ~digitalRead(Pin::DipSwitch3) & 0x1;
+    return ((d3 << 3) | (d2 << 2) | (d1 << 1) | (d0));
+}
+
+}
 
 void setup()
 {
     Serial.begin(115200);
 
+    InitializeDipSwitch();
+    g_OwnAddress = ReadOwnAddress();
+    
     // USB-CDC のせいか起動直後にログを大量に出しても PC 側に表示されない
     // 適当なディレイを入れると安定するようになったので暫定対処
     delay(1000);
+
+    LOG("OwnAddress: 0x%02x\n", g_OwnAddress);
 }
 
 void loop()
